6f24db7f9a5b1891cb74e1543aee014e6801a75f
Atav <atav.daniyalov01@gmail.com>
2024-10-31 18:57:32 +0300

    feat: Добавил страницу 404

---
+++ src/pages/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="error-page">Страница не найдена :)</div>
+</template>
+
+<style lang="scss" scoped>
+.error-page {
+  text-align: center;
+  font-size: 30px;
+  font-weight: 500;
+}
+</style>
